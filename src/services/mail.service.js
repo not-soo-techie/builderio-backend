08896c497d1ba72ced61c7bd7acaa238e90fee54
Yashvendra <yashvendra.singh@newtonschool.co>
2026-02-04 11:57:48 +0530

    Mail fix

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -1,35 +1,4 @@
-// import nodemailer from "nodemailer";
-// import { Resend } from "resend";
-
-// const resend = new Resend(process.env.RESEND_API_KEY);
-// const resend = new Resend('re_FAfMTvKL_CByJRCVSq2FgWs4QTRF2RAdh');
-
 import nodemailer from "nodemailer";
-
-// export const sendEMail = async (options) => {
-//     const transporter = nodemailer.createTransport({
-//         host: process.env.SMTP_HOST,
-//         port: process.env.SMTP_PORT,
-//         service: process.env.SMTP_SERVICE,
-//         auth: {
-//             user: process.env.SMTP_USER,
-//             pass: process.env.SMTP_PASS
-//         },
-//         secure: true
-//     });
-
-//     const mailOptions = {
-//         from: process.env.SMTP_USER,
-//         to: options.email,
-//         subject: options.subject,
-//         text: options.message,
-//         html: options.html
-//     };
-
-//     await transporter.sendMail(mailOptions);
-// }
-
-
 
 export const sendProjectEmail = async (email, name, project) => {
   const html = `
@@ -43,12 +12,12 @@ export const sendProjectEmail = async (email, name, project) => {
 
     <h4>Expected Sections</h4>
     <ul>
-      ${project.sections.map(s => `<li>${s}</li>`).join("")}
+      ${project.sections.map((s) => `<li>${s}</li>`).join("")}
     </ul>
 
     <h4>Creativity Focus</h4>
     <ul>
-      ${project.creativity.map(c => `<li>${c}</li>`).join("")}
+      ${project.creativity.map((c) => `<li>${c}</li>`).join("")}
     </ul>
 
     <p style="margin-top:20px;">
@@ -63,30 +32,33 @@ export const sendProjectEmail = async (email, name, project) => {
   console.log("Sender:", process.env.MAIL_USER);
 
   const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        service: process.env.SMTP_SERVICE,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-        },
-        secure: true
-    });
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+    secure: false,
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-    const mailOptions = {
-        from: process.env.SMTP_USER,
-        to: email,
-        subject: "Your Workshop Project Assignment",
-        // text: options.message,
-        html: html
-    };
+  const mailOptions = {
+    from: process.env.SMTP_USER,
+    to: email,
+    subject: "Your Workshop Project Assignment",
+    // text: options.message,
+    html: html,
+  };
 
-    await transporter.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions);
 
-//   await resend.emails.send({
-//     from: "Workshop Allocator <onboarding@resend.dev>",
-//     to: email,
-//     subject: "Your Workshop Project Assignment",
-//     html,
-//   });
+  //   await resend.emails.send({
+  //     from: "Workshop Allocator <onboarding@resend.dev>",
+  //     to: email,
+  //     subject: "Your Workshop Project Assignment",
+  //     html,
+  //   });
 };
